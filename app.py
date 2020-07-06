14975f6f2cb578a28c62b739cc33dfc1b7995873
onadj <oliver.nad@gmail.com>
2020-07-06 18:37:04 +0000

    move password

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://onadj:Newjob2020@myfirstcluster-hbcie.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://onadj:@myfirstcluster-hbcie.mongodb.net/task_manager?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
 
